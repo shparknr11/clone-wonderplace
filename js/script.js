98d458ae08ef2c8c991175c88595a45fb15e5000
shparknr11 <shparknr11@naver.com>
2024-08-27 16:12:48 +0900

    feat : 원더플레이스 클론코딩 (반응형페이지)

--- js/script.js
+++ js/script.js
@@ -244,3 +244,13 @@ window.onload = function () {
     effectText();
   });
 };
+
+// 팝업창
+window.addEventListener("load", function () {
+  // document.querySelector(".popup-close").addEventListener("click", function(){})
+  const closeBt = document.querySelector(".popup-close");
+  const popup = document.querySelector(".popup");
+  closeBt.addEventListener("click", function () {
+    popup.style.display = "none";
+  });
+});
